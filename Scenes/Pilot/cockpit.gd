df4781248f1879c1c78ec4f766290fb226558f0d
TheWiiUEnthusiast <37639776+TheWiiUEnthusiast@users.noreply.github.com>
2025-07-26 11:40:00 +1200

    added altuituatba

---
+++ Scenes/Pilot/cockpit.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+var current_altitude: int
+var target_altitude: int
+var tilt_angle: int
+var tilt_direction = 1
+
+func _ready():
+	current_altitude = 30000
+	
+func _process(delta):
+	if Input.is_action_pressed('up'):
+		current_altitude += 10
+		tilt_direction = 1
+		tilt_angle = 0
+	else: if Input.is_action_pressed("down"):
+		current_altitude += -10
+		tilt_direction = -1
+		tilt_angle = 0
+	else: 
+		current_altitude += (tilt_angle * tilt_direction)
+	
+	print(current_altitude)
+
+func _on_tilt_timer_timeout() -> void:
+	tilt_angle += 10
