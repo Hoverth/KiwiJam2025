722892ac236b9f41e348cde52329837a84110d44
Shazeel Ali <75409190+ShazeelAli@users.noreply.github.com>
2025-07-27 14:11:37 +1200

    fixedGameOver Synchronisation
    Only letting host change scenes

--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -1,7 +1,11 @@
 extends Control
 
 
+@onready var ReasonText = GameOverReason.gameOverReason
 func _ready() -> void:
-	$ReasonLabel.text = GameOverReason.gameOverReason
+	$ReasonLabel.text = ReasonText
+	if(is_multiplayer_authority()):
+		$Button.disabled = false
+		
 func _on_button_pressed() -> void:
 	MultiplayerSync.change_scene("res://Scenes/Lobby.tscn")
